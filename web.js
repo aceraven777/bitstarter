af0699c72b8f05719614afe286e458eacd1830f7
Arisheo Lacdao <aris@a-solutions.ph>
2013-07-11 10:34:20 +0800

    attempt 3

--- web.js
+++ web.js
@@ -3,9 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	response.send("sdfsdf");
 	var fs = require('fs');
-	response.send(fs.readFileSync("index.html"));
+	response.send(fs.readFileSync("index.html",'utf8'));
 });
 
 var port = process.env.PORT || 5000;
