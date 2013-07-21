1897781de2be297eef8b2d3047666827252e8799
Arisheo Lacdao <aris@a-solutions.ph>
2013-07-21 18:33:53 +0800

    week 4: populate content to the page

--- web.js
+++ web.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "index.html";
+var url = require('url');
+var request_file;
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-	var html = fs.readFileSync(htmlfile).toString();
+app.get('/*',function(request, response) {
+	if(!request.params[0])
+	{
+		request_file = "index.html";
+	}
+	else
+	{
+		request_file = request.params[0];
+	}
+
+	if(request.url.indexOf('.css') != -1)
+	{
+		response.header('Content-Type', 'text/css');
+	}
+	else if(request.url.indexOf('.js') != -1)
+	{
+		response.header('Content-Type', 'text/javascript');
+	}
+	else
+	{
+		response.header('Content-Type', 'text/html');
+	}
+
+	var html = fs.readFileSync(request_file).toString();
 	response.send(html);
 });
 
